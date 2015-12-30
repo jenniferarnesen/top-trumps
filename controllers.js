dc39fab0893aebb00ad1fc683d649f8bc93d9edc
Jennifer Arnesen <jennifer.arnesen@pearson.com>
2015-12-30 18:29:40 +0100

    move connection events back to controller - since we are getting rid of

--- controllers.js
+++ controllers.js
@@ -1,6 +1,50 @@
 app.controller('MainController',
-    function ($scope, $timeout, GameEvents, ConnectionEvents, Utils, Storage, Connection) {
-        var callbacks = ConnectionEvents($scope),
+    function ($scope, $timeout, GameEvents, Utils, Storage, Connection) {
+        //var callbacks = ConnectionEvents($scope),
+        var callbacks = {
+                onRegisterIdWithPeerServer: function (id) {
+                    $scope.myId = id;
+                    $scope.status = 'You are registered as ' + id;
+                    $scope.$apply();
+                },
+
+                onPeerConnectToYou: function (conn) {
+                    $scope.connected = true;
+
+                    conn.send({
+                        'type': 'turnCheck',
+                        'value': $scope.myGo
+                    });
+
+                    $scope.status = 'Opponent connected to you!';
+                    $scope.$apply();
+                },
+
+                onDataReceivedFromPeer: function (data) {
+                    $scope.status = 'Data receieved from opponent';
+
+                    switch (data.type) {
+                        case 'turnCheck':
+                            // Check whose turn it is against the handshake data
+                            // hacky as shit but it fucking well works :)
+                            $scope.myGo = !data.value;
+                            break;
+
+                        case 'card':
+                            $scope.$emit(GameEvents.CARD_RECEIVED_FROM_OPPONENT, data.value);
+                            break;
+                    }
+
+                    $scope.$apply();
+                },
+
+                onConnectionToPeerSuccess: function (conn) {
+                    $scope.status    = 'Connected to ' + conn.peer;
+                    $scope.connected = true;
+                    $scope.$emit(GameEvents.READY_TO_PLAY);
+                    $scope.$apply();
+                }
+            },
 
             flipOpponentsCard = function (show) {
                 show = show || false;
